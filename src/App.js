a74e9699e9d700b36a6f1bdf2bf722b68da03f25
Robert Dobyns <robertddobyns@gmail.com>
2022-08-30 18:23:30 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+
+import movieList from './movies.json'
+
+const sortedMovies = () => {
+  let movies = movieList
+  movies.sort((a,b) => {
+    let a1 = a.name
+    let b1 = b.name
+
+    if(a1 < b1) {
+      return -1
+    }
+    if(a1 > b1) {
+      return 1
+    }
+    return 0
+  })
+
+  return movies
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{margin: '0 auto', width: '500px', textAlign: 'center'}}>
+        <table>
+
+          <tr>
+            <th>Name</th>
+            <th>Finished</th>
+            <th>Rating</th>
+          </tr>
+
+          <tbody>
+          {sortedMovies().map(movie => (
+              <tr key={movie.id}>
+                <td>
+                  {movie.name}
+                </td>
+                <td style={{backgroundColor: '#ddd'}}>
+                  {movie.finished ? 'yes' : 'no'}
+                </td>
+                <td>
+                  {movie.rating ? 'liked' : 'disliked'}
+                </td>
+              </tr>
+          ))}
+          </tbody>
+        </table>
+
+      </div>
   );
+}
+
+function Movie(props) {
+
+  const {name, finished, rating} = props.movie
+
+  return (
+      <div>
+        {`${name} | ${finished} | ${rating}`}
+      </div>
+  )
 }
 
 export default App;
