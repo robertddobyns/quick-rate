3839a42fc4102ca52e62142cea5c19c975b24148
Robert Dobyns <robertddobyns@gmail.com>
2022-08-30 21:28:11 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import movieList from './movies.json'
+import appVars from './vars.json'
 
 const sortedMovies = () => {
   let movies = movieList
@@ -20,15 +21,29 @@ const sortedMovies = () => {
   return movies
 }
 
+const getRating = (v) => {
+  switch (v) {
+    case 1:
+      return 'Liked'
+    case 2:
+      return 'Meh'
+    case 3:
+      return 'Disliked'
+    default:
+      return 'N/A'
+  }
+}
+
 function App() {
   return (
-      <div style={{margin: '0 auto', width: '500px', textAlign: 'center'}}>
+      <div style={{margin: '0 auto', width: '500px', textAlign: 'center', position: 'relative'}}>
         <table>
 
           <tr>
             <th>Name</th>
             <th>Finished</th>
             <th>Rating</th>
+            <th>Twitter Review</th>
           </tr>
 
           <tbody>
@@ -38,16 +53,22 @@ function App() {
                   {movie.name}
                 </td>
                 <td style={{backgroundColor: '#ddd'}}>
-                  {movie.finished ? 'yes' : 'no'}
+                  {movie.finished ? 'Yes' : 'No'}
                 </td>
                 <td>
-                  {movie.rating ? 'liked' : 'disliked'}
+                  {getRating(movie.rating)}
+                </td>
+                <td>
+                  {movie.review ? 'Yes' : 'No'}
                 </td>
               </tr>
           ))}
           </tbody>
         </table>
-
+        <div style={{position: 'fixed', bottom: 0,}}>
+          <div>{appVars.version}</div>
+          <div>{appVars.date}</div>
+        </div>
       </div>
   );
 }
